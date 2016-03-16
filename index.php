d5933816aa50bb1b9bab9b58644a1f9164f3245a
root <root@server1.trzl.cl>
2016-03-16 02:24:37 +0000

    Cambio de nombre bienvenido a index.php

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <![endif]-->
+    <title>Terrazul</title>
+    <meta name="description" content="Empresa de asesorias tributarias y contables, amplio conocimiento del rubro nacional e internacional"  lang="ES"/>
+    <meta name="keywords" content="asesoria,impuestos,tributaria,contabilidad,reforma tributaria,beneficios tributarios,Payroll,ayuda pyme,pyme,emprendimiento" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/icono.ico" >
+    
+    <link rel="stylesheet" href="css2/filoxenia.css">
+    <link rel="stylesheet" href="css2/magnific-popup.css">
+
+    <link rel="stylesheet" href="css2/custom.css">
+
+    <script src="js/vendor/custom.modernizr.js"></script>
+    <style type="text/css">
+        .imagen{
+            width: 75px;
+            height: 75px; 
+            font-size: x-large; 
+            font: bold;
+        }
+        .texto{
+            text:black;
+        }
+        
+    </style>
+</head>
+
+<body>
+    <header class="contain-to-grid fixed">
+        <div class="row">
+            <div class="large-12 column">
+                <nav id="menu" class="top-bar ">
+
+                    <ul class="title-area">
+                        <li class="name">
+                            <p style="color:white">                               
+                                    <a href="bienvenido.php" >
+                                        <img src="images/logo.png" alt="logo" class="imagen" align="center">
+                                    </a>
+                            Terrazul </p>
+                        </li>
+                        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a>
+                        </li>
+                    </ul>
+
+                    <section class="top-bar-section">
+                        <ul class="right">
+                            <li><a href="bienvenido.php">Bienvenido</a>
+                            </li>
+                            <li><a href="servicios.php">Servicios</a>
+                            </li>
+                            <li><a href="contacto.php">Contacto</a>
+                            </li>
+                            <li><a href="intranet/index.php">Intranet</a>
+                            </li>
+                        </ul>
+                    </section>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <section id="main" role="main">
+
+        <section id="hero">
+            <div class="row">
+                <div class="large-10 large-centered column text-center   ">
+                    <div id="hero-message" class="fadeInDown animated">
+                        <h1 style="color: #5882FA;"><bold>Terrazul Asesorias Empresariales</bold></h1>
+                        <h4 style="color: #FFFF00;" >Que la Reforma Tributaria no se convierta en un dolor de cabeza.  Te ofrecemos distintas alternativas para dar un oportuno cumplimiento contable y tributario, tanto a personas como empresas.</h4>
+                        <br><br><br><br><br><a href="servicios.php" class="button">Ver servicios</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        
+
+        <section class="part">
+            
+            <div class="row">
+                <div class="large-12 column">
+                    <div class="row title">
+                <div class="large-12 column">
+                    <h6>SERVICIOS</h6>
+                </div>
+            </div>
+                    
+                    <p align="justify">
+                        Contamos con servicios especializados en Contabilidad, Impuestos y Remuneraciones, para lo cual hemos dise&#241;ado distintos planes de acuerdo a sus necesidades.
+                    </p>
+                    
+                    <h5  >Contabilidad</h5>
+                    <p align="justify">
+                        Simplificamos el proceso contable, el Cliente env&#237;a v&#237;a web los documentos y nosotros procesamos su contabilidad. 
+                    </p>
+                    
+                    <h5  >Impuestos</h5>
+                    <p align="justify">
+                       De forma f&#225;cil y expedita, una vez se procesan los documentos en Contabilidad, procedemos a calcular los impuestos y los ingresamos al sitio web del SII. Ud. s&#243;lo tiene que verificar los valores y pagar directamente en el sitio web.
+ 
+                    </p>
+                    
+                    <h5  >Remuneraciones</h5>
+                    <p align="justify">
+                        Mensualmente procesamos las remuneraciones de su empresa, le enviamos las liquidaciones de sueldo y el libro de remuneraciones.  Autom&#225;ticamente enviamos la informaci&#243;n a Previred y Ud. se dedica a pagar sus remuneraciones y posteriormente cancela las cotizaciones previsionales en portal Previred.
+                    </p>
+                    
+                </div>
+            </div>
+            <br><br>
+            
+            
+        </section>
+
+        
+
+    <a id="back-top" href="#"><i class="icon-caret-up"></i></a>
+
+    <footer>
+        <div class="row spacy">
+            <div class="large-6 column">
+                <h4>Sobre Nosotros</h4>
+                <br>
+                 <div id="map-canvas">
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.5989580526425!2d-70.56971589999999!3d-33.407625599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cedee6357ec7%3A0x5f5b7a4b7ec392d5!2sAlonso+de+C%C3%B3rdova+5870%2C+Las+Condes%2C+Regi%C3%B3n+Metropolitana+de+Santiago!5e0!3m2!1ses-419!2scl!4v1432235668983" width="450" height="225" frameborder="0" style="border:0"></iframe>
+
+           			 </div>
+		</div>
+        
+
+            <div class="large-6 column">
+                <h4>Contacto</h4>
+
+                <div class="row">
+                    <div class="small-3 column">
+                        <h6>Direcci&#243n:</h6>
+                    </div>
+                    <div class="small-9 column">
+                        <p>Casa matriz: Alonso de Cordova Nº 5870, oficina 724, Las condes</p>
+                        <p>Sucursal: Lord Cochrane Nº 30, oficina 901, Santiago Centro </p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="small-3 column text">
+                        <h6>Telef&#243no:</h6>
+                    </div>
+                    <div class="small-9 column text">
+                        <p>+56 2 2725 8472</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="small-3 column text">
+                        <h6>Email:</h6>
+                    </div>
+                    <div class="small-9 column text">
+                        <p>
+                            <a href="mailto:contacto@terrazul-chile.cl"><img src="correo2.jpg"></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="large-12 column">
+                <hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <p class="small-12 large-4 large-uncentered column copyright">
+                Copyright &copy; 2014 Terrazul-Chile
+            </p>
+
+<!--            <p class="small-12 large-8 column social">
+                <a href="mailto:info@filoxenia.com"><i class="icon-envelope"></i></a>
+                <a href="#"><i class="icon-rss"></i></a>
+                <a href="//www.facebook.com" target="_blank"><i class="icon-facebook"></i></a>
+                <a href="//www.twitter.com" target="_blank"><i class="icon-twitter"></i></a>
+                <a href="//www.google.com/plus" target="_blank"><i class="icon-google-plus"></i></a>
+                <a href="//www.linkedin.com" target="_blank"><i class="icon-linkedin"></i></a>
+                <a href="skype:echo123?call" target="_blank"><i class="icon-skype"></i></a>
+            </p>-->
+        </div>
+
+    </footer>
+
+    <!-- Javascript -->
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/jquery.magnific-popup.js"></script>
+    <script src="js/foundation/foundation.js"></script>
+    <script src="js/foundation/foundation.topbar.js"></script>
+    <script src="js/foundation/foundation.section.js"></script>
+    <script src="js/filoxenia.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
